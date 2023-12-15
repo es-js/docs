863efa5f7a1b610d67160906a34e40188160cf05
enzonotario <enzo.notario@gmail.com>
2023-12-15 20:13:45 -0300

    chore(playground) link

--- src/.vitepress/theme/components/index/Playground.vue
+++ src/.vitepress/theme/components/index/Playground.vue
@@ -9,7 +9,12 @@
       </h3>
       <section class="w-full flex flex-col justify-center items-center gap-6 my-6 h-[33rem]">
         <iframe src="https://editor.esjs.dev?embed=true" width="100%" height="100%" loading="lazy" class="rounded border border-gray-300 dark:border-gray-700" />
-        <a href="https://editor.esjs.dev" class="action primary-soft space-x-2">
+        <a
+          href="https://editor.esjs.dev"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="action primary-soft space-x-2"
+        >
           <span>Abrir EsJS Editor</span>
           <span class="i-mdi-open-in-new" />
         </a>
